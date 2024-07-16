adb1fdf085f9744e6f9fbe9496c03cf249c8765a
newbie1223 <taiti7919@gmail.com>
2024-07-17 05:52:38 +0900

    make keyword_search

---
+++ keyword_search.py
@@ -0,0 +1,15 @@
+import csv
+
+keyword = 'コク'
+
+l = []
+i=0
+with open('./reviews/reviews_clear-asahi.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        for x in row:
+            if keyword in x:
+                l.append(x)
+                i+=1
+print(l)
+print(i)
