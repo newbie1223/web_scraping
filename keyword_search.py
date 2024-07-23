974c0b0c046b1afc238efe4c8875cd4edf0977fa
newbie1223 <taiti7919@gmail.com>
2024-07-23 14:28:16 +0900

    make review_com

--- keyword_search.py
+++ keyword_search.py
@@ -1,15 +1,17 @@
 import csv
 
-keyword = '甘み'
+keyword1 = 'コク'
+keyword2 = 'ある'
 
 l = []
 i=0
-with open('./reviews/reviews_clear-asahi.csv') as f:
+with open('./reviews/reviews_asahi-super-dry.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         for x in row:
-            if keyword in x:
-                l.append(x)
-                i+=1
+            if keyword1 in x:
+                if keyword2 in x:
+                    l.append(x)
+                    i+=1
 print(l)
 print(i)
---
+++ review_com.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request as req
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import datetime
+from datetime import date
+from datetime import timedelta
+import time
+import csv
+
+# Google Chromeを起動
+browser = webdriver.Chrome()
+# Chrome find wait
+browser.implicitly_wait(10)
+
+# ページにアクセス
+urlName = "https://review.kakaku.com/review/K0000661313/#tab"
+browser.get(urlName)
+
+x = 1
+while x <= 10:
+    try:
+        # 要素が存在するまで待つ
+        element = WebDriverWait(browser, 20).until(
+            EC.presence_of_element_located((By.XPATH, f'/html/body/div[1]/div/div/div[4]/div[5]/div/div[2]/div[1]/p/a[{x}]'))
+        )
+        print("Element is found")
+        # 要素がクリック可能になるまで待つ
+        # element = WebDriverWait(browser, 30).until(
+        #     EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[3]/div[2]/div/div[2]/div/div[11]/a/span'))
+        # )
+        # 要素が画面内に表示されるようにスクロール
+        browser.execute_script("arguments[0].scrollIntoView(true);", element)
+        time.sleep(1)  # スクロール後に待機時間を設ける
+        # JavaScript Executorを使ってクリック
+        browser.execute_script("arguments[0].click();", element)
+        print('click')
+        x += 1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        break
+
+time.sleep(3)
+# ページのHTMLを取得
+# response = req.urlopen(urlName)
+url = BeautifulSoup(browser.page_source, 'html.parser')
+
+# 特定のクラスを持つすべての要素を検索
+reviews = url.find_all("p", class_="revEntryCont")
+
+# 各トレンド記事を表示
+# for review in reviews:
+#     print(review.text)
+
+with open('reviews.csv', 'w', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow([review.text for review in reviews])
+
+df = pd.read_csv('reviews.csv')
+print(df)
+
+browser.quit()
